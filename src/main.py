0a1b12f7eb41d8f7db0145132d9395cc3cb108fe
AlirezaKeshavarz83 <47504151+AlirezaKeshavarz83@users.noreply.github.com>
2021-10-24 15:52:45 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -32,31 +32,32 @@ def codeforces(s):
                 return 0
             j = i
     for i in range(len(s)):
-        if i == j or (not '0' <= s[i] <= '9'):
+        if i != j and (not '0' <= s[i] <= '9'):
             return 0
-    return "https://codeforces.com/contest/" + s[:j] + "/problem/" + s[j:]
+    return "https://codeforces.com/contest/" + s[:j] + "/problem/" + s[j:].upper()
 def atcoder(s):
-    if(len(s) < 5):
+    s = s.lower()
+    if(len(s) < 5 or len(s) > 7):
         return 0
     if(s[0] != 'a' or s[2] != 'c'):
         return 0
-    if(not ('a' <= s[-1] <= 'z' or 'A' <= s[-1] <= 'Z')):
+    if(not 'a' <= s[-1] <= 'z'):
         return 0
     for i in range(3, len(s) - 1):
         if(not '0' <= s[i] <= '9'):
             return 0
+    s = s[:3] + ('0' * (7 - len(s))) + s[3:]
     return "https://atcoder.jp/contests/" + s[:-1] + "/tasks/" + s[:-1] + "_" + s[-1]
     
 
 token = os.getenv("token")
-print('"' + token + '"')
 updater = Updater(token, use_context=True)
 admin = os.getenv("admin")
 
 def start(update : Update, context : CallbackContext):
     user = update.message.from_user
     user_id = user.id
-    context.bot.send_message(chat_id=admin, text= ": <a href=\"tg://user?id=" + user_id + "\">" + user.full_name + "</a>", parse_mode="HTML")
+    context.bot.send_message(chat_id=admin, text= "<a href=\"tg://user?id=" + str(user_id) + "\">" + user.full_name + "</a>", parse_mode="HTML")
     update.message.reply_text("سلام")
 
 def handle(update : Update, context : CallbackContext):
